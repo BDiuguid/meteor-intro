d280d617d8f14927c868123c8a3648c135ce7ce4
Ben Diuguid <Bendiuguid@gmail.com>
2015-07-14 09:04:55 -0400

    fixed parsed rimeRemaining

--- theButton/client/templates/clickLog/clickLog.js
+++ theButton/client/templates/clickLog/clickLog.js
@@ -1,5 +1,12 @@
 Template.clickLog.helpers({
   clickRecords: function() {
     return Clicks.find();
+  },
+  timeRemainingParsed: function() {
+    var rem = Math.round(this.timeRemaining / 1000);
+    if (rem < 10) {
+      rem = "0" + rem;
+    }
+    return rem;
   }
 });
